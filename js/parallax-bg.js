2c6c6ce1625a48e3bb61d0a23a6264883fe1de8e
daetoyahorosho <35369946+daetoyahorosho@users.noreply.github.com>
2018-03-09 22:12:59 +0500

    Update parallax-bg.js

--- js/parallax-bg.js
+++ js/parallax-bg.js
@@ -1,14 +1,14 @@
 $.fn.parallax = function(resistance, mouse) {
   $el = $(this);
-  TweenLite.to($el, 0.2, {
+  TweenLite.to($el, 0.3, {
     x: -((mouse.clientX - window.innerWidth / 2) / resistance),
     y: -((mouse.clientY - window.innerHeight / 2) / resistance)
   });
 };
 
 $(document).mousemove(function(e) {
-  $(".background").parallax(30, e);
-  $(".cloud1").parallax(10, e);
-  $(".cloud2").parallax(20, e);
-  $(".cloud3").parallax(30, e);
+  $(".background").parallax(10, e);
+  $(".cloud1").parallax(15, e);
+  $(".cloud2").parallax(28, e);
+  $(".cloud3").parallax(240, e);
   });
